0ba2909638e878da15dc66406e177cea8f3df573
kml111 <2166964968@qq.com>
2020-08-27 15:55:10 +0800

    完成遍历省�å¸县三级列表的功能

--- app/src/main/java/com/coolweather/android/db/City.java
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -4,8 +4,18 @@ import org.litepal.crud.DataSupport;
 
 public class City extends DataSupport {
     private int id;
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
     private String cityName;
+    private int cityCode;
     private int provinceId;
+
+    public int getCityCode() {
+        return cityCode;
+    }
 
     public int getId() {
         return id;
